605ccccc332ccc615a3a237f5e588362d4a2bbd8
EDUARDO MENDES <windowsxpedro@gmail.com>
2020-10-14 22:34:00 -0300

    Orph detail

--- web/src/pages/Orphs.tsx
+++ web/src/pages/Orphs.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-import {FiPlus} from 'react-icons/fi'
-import {Map, TileLayer} from 'react-leaflet'
+import {FiPlus, FiArrowRight} from 'react-icons/fi'
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
+import Leaflet from 'leaflet'
 
 import 'leaflet/dist/leaflet.css'
 import placePicker from '../assets/place-picker.svg';
 import '../styles/pages/orphs.css'
+
+const mapIcon = Leaflet.icon({
+    iconUrl: placePicker,
+
+    iconSize:[58, 68],
+    iconAnchor: [29, 68], 
+
+    popupAnchor: [170, 2]
+})
 
 const Orphs: React.FC = () => {
 
@@ -26,9 +36,17 @@ const Orphs: React.FC = () => {
         </aside>
         <Map className="map" center={[-27.5960216,-48.565173]} zoom={14.67} style={{width:'100%', height:'100%'}}>
             <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+            <Marker icon={mapIcon} position={[-27.5960216,-48.565173]}>
+                <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
+                    Orfanato são josé
+                    <Link to='/orphanages/1'>
+                    <FiArrowRight size={20} color='#FFF'/>
+                    </Link>
+                </Popup>
+            </Marker>
         </Map>
 
-        <Link to="" className="create-orphanage">
+        <Link to="/orphanages/create" className="create-orphanage">
             <FiPlus size={32} color="#FFF" />
         </Link>
     </div>
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Home from './pages/Home'
 import Orphs from './pages/Orphs'
+import Orphanage from './pages/Orphanage'
+import CreateOrphanage from './pages/CreateOrphanage'
 
 export default () => {
     return (
         <BrowserRouter>
-            <Route exact path="/" >
-                <Home />
-            </Route>
-            <Route path="/orphanages">
-                <Orphs />
-            </Route>
+            <Switch>
+                <Route exact path="/" >
+                    <Home />
+                </Route>
+                <Route exact path="/orphanages/create">
+                    <CreateOrphanage />
+                </Route>
+                <Route exact path="/orphanages/:id">
+                    <Orphanage />
+                </Route>
+                
+                <Route exact path="/orphanages">
+                    <Orphs />
+                </Route>
+                
+                
+            </Switch>      
         </BrowserRouter>
     )
 }
